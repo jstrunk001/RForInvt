9a8682480dd078e81a1d9f1cb4687a2deb78d9fc
Jacob Strunk <strunky@gmail.com>
2021-10-23 18:21:32 -0700

    update code to enable paths with spaces

--- R/fvs_prototype_keyfile.R
+++ R/fvs_prototype_keyfile.R
@@ -37,15 +37,13 @@
 #'
 #'@examples
 #'
-#'
 #' clus1 = parallel::makeCluster(4)
 #'
 #'#assume a typical inventory dataset and prepare fvs parameters
 #' stand_data = data.frame(stand=1:10, year=2000:2009)#'
 #' df_params = fvs_protype_params()
 #' df_params[1:nrow(stand_data),]=NA
-#' df_params[,"stand_id"] = stand_data$stand
-#' df_params[,"stand_cn"] = stand_data$stand
+#' df_params[,"cn"] = stand_data$stand
 #' df_params[,"invyr"] = stand_data$year
 #' df_params[,"timeint"] = 1
 #' df_params[,"numcycle"] = 1
@@ -59,13 +57,11 @@
 #' key_proto = fvs_prototype_keyfile(invyr = "InvYear       2001", notriple=NULL)
 #'
 #'#convert prototype key file into series of key files associated with each cn
-#' df_keys = fvs_make_keyfiles(df_params, key_proto = key_proto, cluster = clus1)
+#' df_keys = fvs_make_keyfiles(df_params, key_proto = key_proto, cluster = clus1, id="cn")
 #'
 #'#lastly, actually run fvs
 #' fvs_run(df_keys, cluster = clus1)
-#'
 #' parallel::stopCluster(clus1)
-#'
 #'
 #'@export
 #
@@ -106,7 +102,7 @@ fvs_prototype_keyfile = function(
         ,other_keywords
         ,"DataBase"
         ,"DSNOut"
-        ,"@output_db@"
+        ,"@output_db_q@"
         ,"* FVS_Summary, FVS_Compute"
         ,"Summary        2"
         ,"Compute            0         1"
@@ -124,7 +120,7 @@ fvs_prototype_keyfile = function(
         ,"!Exten:base Name:From: FVS_GroupAddFilesAndKeywords"
         ,"Database"
         ,"DSNIn"
-        ,"@input_db@"
+        ,"@input_db_q@"
         ,"StandSQL"
         ,"SELECT * FROM FVS_StandInit"
         ,"WHERE Stand_ID= '%StandID%'"
