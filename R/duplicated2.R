1a9a7d98a676b01afdca4aca60e750895cefaa02
Jacob <jacob.strunk@usda.gov>
2023-05-12 12:05:00 -0700

    various updates

---
+++ R/duplicated2.R
@@ -0,0 +1,50 @@
+#'
+#'@title
+#' same as duplicated but argument to show all duplicates
+#'
+#'@description
+#' same as duplicated but argument to show all duplicates
+#'
+#'@details
+#' same as duplicated but argument to show all duplicates
+#'
+#'\cr
+#'Revision History
+#' \tabular{ll}{
+#'1.0 \tab 2023 May 12 Created\cr
+#'}
+#'
+#'@author
+#'
+#'Jacob Strunk <strunky@@gmail.com>
+#'
+#'@param ... regular arguments to duplicated
+#'@param all T/F return all duplicates
+#'
+#'@return
+#'  vector of duplicates
+#'
+#'@examples
+#'
+#'
+#'@export
+#
+#'@seealso \code{\link{duplicated}}\cr
+
+duplicated2 = function(
+  ...
+  ,all = T
+){
+
+  if(all){
+
+    duplicated(...) | duplicated(...,fromLast=T)
+
+  }else{
+
+    duplicated(...)
+
+  }
+
+}
+
