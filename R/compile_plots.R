718223557a5f2cadcb809422caf76b87c8c45f69
Jacob <jacob.strunk@usda.gov>
2024-10-09 17:31:12 -0700

    add consistency to tree compilation naming convention

--- R/compile_plots.R
+++ R/compile_plots.R
@@ -25,22 +25,26 @@
 #'
 #'@author
 #'
-#'Jacob Strunk <Jstrunk@@fs.fed.us>
+#'Jacob Strunk <someone@@somewhere.com>
 #'
 #'
 #'@param df_tree data frame of tree records
 #'@param df_plot data frame of plot records
-#'@param df_tree_nms map expected tree column names onto df_tree. These are the expected names: c( plot_ids = "?", tr_ids = c("?","?") , dbh = "?" , ht = "?" , spcd = "?" , tr_wt = "?"). Feel free to provide others used by custom functions.
-#'@param df_plot_nms map expected plot column names onto df_plot. These are the expected names: c( plot_ids = c("?","?"), plt_wt = c(NA,"?")[1] ). Feel free to provide others used by custom functions.
+#'@param tree_nms map expected tree column names onto df_tree. These are the expected names: c( plot_ids = "?", tr_ids = c("?","?") , dbh = "?" , ht = "?" , spcd = "?" , tr_wt = "?"). Feel free to provide others used by custom functions.
+#'@param plot_nms map expected plot column names onto df_plot. These are the expected names: c( plot_ids = c("?","?"), plt_wt = c(NA,"?")[1] ). Feel free to provide others used by custom functions.
 #'@param plot_filter sqldf string to filter out plots
 #'@param tree_filter sqldf string to filter out trees
 #'@param dir_out where to write results of plot level compilation
-#'@param fns_compute list of functions to compute plot level attributes. Functions typically accepting plot level tree list and list of tree / plot names and must return a data.frame. Functions must accept ... argument.
-#'@param ... additional arguments provided to fns_compute e.g textString="hello" argument in silly function someFun = function(tl,tlNms,textString){data.frame(note=textString)}
+#'@param fns_compute list of optional functions to compute plot level attributes. Functions typically accept plot level tree list (df_tree) plot list (df_plot). Optional compile functions must accept '...' arguments, e.g. function(df_tree, ...)
 #'@param return return compiled data
 #'@param nclus if nclus > 1 then nclus parallel nodes are split off to process in parallel
-#'
 #'@param do_debug stop and debug function
+#'
+#'@param ... additional arguments provided to fns_compute e.g textString="hello" argument in silly function someFun = function(tl,tlNms,textString){data.frame(note=textString)}
+#'@param sum_nms <...> argument passed to optional plot_wtsum function,  passed generically by compile_plots through '...'
+#'@param n_dom_spp <...>  argument passed to optional spp_y_plot function,  passed generically by compile_plots through '...'
+#'@param spp_y <...>  argument passed to optional spp_y_plot function,  passed generically by compile_plots through '...'
+
 #'
 #'@return
 #'  NULL (return=F) or a data.frame of plot attributes (return=T)
@@ -63,39 +67,45 @@
 #'   )
 #'
 #'   #compute tree level attributes for fake tree list
-#'   testTL =
-#'     compileTrees(
+#'   test_tl =
+#'     compile_trees(
 #'       df_fake
-#'       ,trID = "trid"
-#'       ,sppNm = "spp"
-#'       ,dbNm = "db"
-#'       ,htNm = "ht"
-#'       ,dbclNm = "dbcl"
-#'       ,dbcl = c(seq(0,32,4),50,1000)
-#'       ,dbclY = c("ba_ft")
-#'       ,spp_y = c("ba_ft")
-#'       ,sppDbclY = c("ba_ft")
-#'       ,acresNm = "acres"
-#'       ,nTreesNm = NA
 #'
-#'       ,fns_compute =
+#'       #arguments to fn_compute functions
+#'       ,tr_id = "trid"
+#'       ,spp_nm = "spp"
+#'       ,db_nm = "db"
+#'       ,htNm = "ht"
+#'       ,dbcl_nm = "dbcl"
+#'       ,dbcl = c(seq(0,32,4),50,1000)
+#'       ,dbcl_y = c("ba_ft")
+#'       ,spp_y = c("ba_ft")
+#'       ,spp_dbcl_y = c("ba_ft")
+#'       ,acres_nm = "acres"
+#'       ,ntrees_nm = NA
+#'
+#'       #optional functions to run against tree data
+#'       #must accept ...
+#'       ,fn_compute =
 #'         list(
 #'           tpa
 #'           ,ba_ft
 #'           ,dbcl
-#'           ,dbclY
+#'           ,dbcl_y
 #'           ,spp_y
-#'           ,dbclspp_y
+#'           ,dbcl_spp_y
 #'         )
+#'
 #'     )
 #'
 #'   testTL
+#'
 #'
 #'   #compute plot level attributes from fake tree list
 #'   res_pl =   compile_plots(
 #'
 #'     df_tree = testTL
-#'     ,df_tree_nms = c(plot_ids = c("pltId") , tr_ids = c("trid") , dbh = "db" , ht = "ht" , spcd = "spp" , tr_wt = "TPA" )
+#'     ,tree_nms = c(plot_ids = c("pltId") , tr_ids = c("trid") , dbh = "db" , ht = "ht" , spcd = "spp" , tr_wt = "TPA" )
 #'     ,dir_out= file.path("c:/temp/RSForInvt/Compile",format(Sys.Date()))
 #'     ,fns_compute = list(
 #'       plot_wtmn
@@ -108,7 +118,7 @@
 #'     ,nclus = 1
 #'
 #'     #' arguments to custom functions - in this case plot_wtsum
-#'     ,vSumNm = c("TPA",grep("^ba",names(testTL),value=T))
+#'     ,sum_nms = c("TPA",grep("^ba",names(testTL),value=T))
 #'
 #'   )
 #'
@@ -117,7 +127,7 @@
 #'
 #'@import plyr parallel sqldf reshape2 RSQLite flock DBI
 #'
-#'@seealso \code{\link{compileTrees}}\cr \code{\link{parLapplyLB}}\cr
+#'@seealso \code{\link{compile_trees}}\cr \code{\link{parLapplyLB}}\cr
 
 #updates to do:
 #	write FIA data processing wrappers
@@ -129,12 +139,14 @@
 compile_plots=function(
 
   cl = NA
-  ,df_tree = data.frame(PLT_CN = 1, year = 2020 , dbh = NA , spcd = NA , ac = 0.1  )
-  ,df_plot = list( NA, data.frame (PLT_CN=c(1,2) , PLOT = 1 , YEAR = 2020, STATE =1 , COUNTY = 1 , PROJECT = "Test"))[1]
-  ,df_tree_nms = list( plot_ids = c("PLOT", "YEAR") , tr_ids = c("tr_cn") , dbh = "DIA" , ht = "HT" , spcd = "SPCD" , tr_wt = "TPA" )
-  ,df_plot_nms = list(plot_ids = c( "STATE" , "COUNTY" , "PROJECT" , "PLOT" , "YEAR" ), plt_wt = NA )
-  ,plot_filter = c(NA, "select * from df_plot where YEAR = 2018 and STATE = 'WA' and CONDITION = 1")
+  ,df_tree = list( NA, data.frame(PLT_CN = 1, year = 2020 , dbh = NA , spcd = NA , ac = 0.1  ))[[1]]
+  ,df_plot = list( NA, data.frame(PLT_CN=c(1,2) , PLOT = 1 , YEAR = 2020, STATE =1 , COUNTY = 1 , PROJECT = "Test"))[[1]]
+
+  ,tree_nms = list( plot_ids = c("PLOT", "YEAR") , tr_ids = c("tr_cn") , dbh = "DIA" , ht = "HT" , spcd = "SPCD" , tr_wt = "TPA" )
+  ,plot_nms = list(plot_ids = c( "STATE" , "COUNTY" , "PROJECT" , "PLOT" , "YEAR" ), plt_wt = NA )
+
   ,tree_filter = c(NA, "select * from df_tree where dbh > 2 ")
+  ,plot_filter = c(NA, "select * from df_plot where YEAR = 2018 and STATE = 'WA' and CONDITION = 1")
 
   ,dir_out= file.path("c:/temp/RSForInvt/Compile",format(Sys.Date()))
   ,nm_out = "compile_plots"
@@ -189,7 +201,7 @@ compile_plots=function(
 
         #read existing ID records
         con_i = DBI::dbConnect(RSQLite::SQLite(), path_sqlite)
-          qry_ids = paste("select",paste(df_tree_nms$plot_ids,collapse=", "),"from",sql_tbl)
+          qry_ids = paste("select",paste(tree_nms$plot_ids,collapse=", "),"from",sql_tbl)
           df_ids_sqlite = DBI::dbGetQuery(con_i,qry_ids)
         DBI::dbDisconnect(con_i)
 
@@ -198,8 +210,8 @@ compile_plots=function(
 
         #filter new trees
         #from https://stat.ethz.ch/pipermail/r-help/2007-January/123488.html
-        df_tree = df_tree[!is.element(interaction(df_tree[,df_tree_nms$plot_ids],drop=T), interaction(unq_id_db[,df_tree_nms$plot_ids],drop=T) ) , ]
-        df_plot = df_plot[!is.element(interaction(df_plot[,df_tree_nms$plot_ids],drop=T), interaction(unq_id_db[,df_tree_nms$plot_ids],drop=T) ) , ]
+        df_tree = df_tree[!is.element(interaction(df_tree[,tree_nms$plot_ids],drop=T), interaction(unq_id_db[,tree_nms$plot_ids],drop=T) ) , ]
+        df_plot = df_plot[!is.element(interaction(df_plot[,tree_nms$plot_ids],drop=T), interaction(unq_id_db[,tree_nms$plot_ids],drop=T) ) , ]
 
     } #end filter records from data.base
     if(do_debug) print("completed filter existing records")
@@ -209,23 +221,23 @@ compile_plots=function(
   dir_out = dir_out[1]
 
 	#grab IDs for tree records
-	plot_ids_in = df_tree_nms[["plot_ids"]]
+	plot_ids_in = tree_nms[["plot_ids"]]
 
 	if(!is.na(dir_out)) if(!dir.exists(dir_out)) dir.create( dir_out , recursive = T )
 
 	#check that tree / plot datasets have matching id fields
-	tr_ids_ok = df_tree_nms[["plot_ids"]] %in% names(df_tree)
-	if(sum(!tr_ids_ok) > 0 ) stop(paste("df_tree does not have some plot_ids:",df_tree_nms[["plot_ids"]][!tr_ids_ok]))
+	tr_ids_ok = tree_nms[["plot_ids"]] %in% names(df_tree)
+	if(sum(!tr_ids_ok) > 0 ) stop(paste("df_tree does not have some plot_ids:",tree_nms[["plot_ids"]][!tr_ids_ok]))
 
 	#check for matching tree an dplot ids
 	if(!no_df_plot){
-  	pl_ids_ok = df_plot_nms[["plot_ids"]] %in% names(df_plot)
-  	if(mean(pl_ids_ok) < 1 ) stop(paste("df_plot does not have some plot_ids:",df_plot_nms[["plot_ids"]][!pl_ids_ok]))
+  	pl_ids_ok = plot_nms[["plot_ids"]] %in% names(df_plot)
+  	if(mean(pl_ids_ok) < 1 ) stop(paste("df_plot does not have some plot_ids:",plot_nms[["plot_ids"]][!pl_ids_ok]))
 	}
 
-	#check that tree dataset matches all names in df_tree_nms
-	tr_nms_ok = unlist(df_tree_nms) %in% names(df_tree)
-	if(sum(!tr_nms_ok) > 0 ) stop(paste("column names in df_tree don't match names provided in df_tree_nms:",unlist(df_tree_nms)[!tr_nms_ok]))
+	#check that tree dataset matches all names in tree_nms
+	tree_nms_ok = unlist(tree_nms) %in% names(df_tree)
+	if(sum(!tree_nms_ok) > 0 ) stop(paste("column names in df_tree don't match names provided in tree_nms:",unlist(tree_nms)[!tree_nms_ok]))
 
 	if(do_debug) print("completed test for agreement between plot and tree tables")
 
@@ -266,7 +278,7 @@ compile_plots=function(
 	#stub
 	#to do - update code to not compile plots with bad tree ids ?
 	# if(F){
-	# 	no_tr = is.na(dat_in[tr_nms["id"],])
+	# 	no_tr = is.na(dat_in[tree_nms["id"],])
 	# 	dat_notree = dat_in[no_tr,]
 	# 	dat_tree = dat_in[!no_tr,]
 	# }
@@ -284,9 +296,9 @@ compile_plots=function(
   		res_i = lapply(
   		    spl_ids
   		  , .compile_1plot
-  		  , trs = dat_in
-  		  , tr_nms = df_tree_nms
-  		  , plot_nms = df_plot_nms
+  		  , dat_tree = dat_in
+  		  , tree_nms = tree_nms
+  		  , plot_nms = plot_nms
   		  , fns_compute = fns_compute
   		  , path_sqlite = path_sqlite
   		  , sql_tbl = sql_tbl
@@ -295,7 +307,7 @@ compile_plots=function(
   		  , ...
   		  )
 
-		if(F) res_i = lapply(spl_ids[1:3] , .compile_1plot , trs = dat_in,  tr_nms = df_tree_nms , plot_nms = df_plot_nms, fns_compute = fns_compute ,dir_out=dir_out, ... )
+		if(F) res_i = lapply(spl_ids[1:3] , .compile_1plot , dat_tree = dat_in,  tree_nms = tree_nms , plot_nms = plot_nms, fns_compute = fns_compute ,dir_out=dir_out, ... )
 
 		if(do_debug) print("completed - process tree lists by plot in linear mode (not parallel) ")
 
@@ -312,9 +324,9 @@ compile_plots=function(
   		    clus_in
   		  , spl_ids
   		  , .compile_1plot
-  		  , trs = dat_in
-  		  , tr_nms = df_tree_nms
-  		  , plot_nms = df_plot_nms
+  		  , dat_tree = dat_in
+  		  , tree_nms = tree_nms
+  		  , plot_nms = plot_nms
   		  , fns_compute = fns_compute
   		  , path_sqlite = path_sqlite
   		  , sql_tbl = sql_tbl
@@ -351,8 +363,8 @@ compile_plots=function(
 #compile tree list form one plot
 .compile_1plot=function(
   id
-  ,trs
-  ,tr_nms
+  ,dat_tree
+  ,tree_nms
   ,plot_nms
   ,sql_tbl
   ,fns_compute
@@ -364,17 +376,17 @@ compile_plots=function(
 ){
 
   #this plot ids with trees, eliminating trees not on this plot
-  trs_i = merge(y=trs ,x=id, by = tr_nms[["plot_ids"]], all.x = T, all.y=F)
+  trs_i = merge(y=dat_tree ,x=id, by = tree_nms[["plot_ids"]], all.x = T, all.y=F)
 
   #get grouping variables as seed columns with id fields
 
-    trs_id = trs_i[1, tr_nms[["plot_ids"]]]
+    trs_id = trs_i[1, tree_nms[["plot_ids"]]]
 
     #iterate through compute functions and append dataframes horizontally
     for(i in 1:length(fns_compute)){
       fni = fns_compute[[i]]
       #if(nrow(trs_i) > 0 ) browser()
-      resi = try(fni(trs = trs_i, tr_nms = tr_nms , ... ))
+      resi = try(fni(dat_tree = trs_i, tree_nms = tree_nms , ... ))
       if(class(resi) == "try-error") return(NULL)
 
       #append tree id fields onto single plot's results
@@ -453,24 +465,24 @@ compile_plots=function(
 #'@export
 #'@rdname compile_plots
 plot_wtsum = function(
-	trs
-	,tr_nms
-	,vSumNm
+	dat_tree
+	,tree_nms
+	,sum_nms
 	,...
 ){
 	#catch records without any actual trees - and set to zero
-	bad_ids = is.na(trs[,tr_nms[["plot_ids"]]])
-	tr_in = trs[!bad_ids,]
-	sumNm_in = vSumNm[vSumNm %in% names(tr_in)]
+	bad_ids = is.na(dat_tree[,tree_nms[["plot_ids"]]])
+	tr_in = dat_tree[!bad_ids,]
+	sum_nm_in = sum_nms[sum_nms %in% names(tr_in)]
 
-	if(length(vSumNm) != length(sumNm_in)) warning("not all columns provided to plot_wtsum with argument 'vSumNm' are present in table 'trs' - only columns present were summed")
+	if(length(sum_nms) != length(sum_nm_in)) warning("not all columns provided to plot_wtsum with argument 'sum_nms' are present in table 'dat_tree' - only columns present were summed")
 	if(nrow(tr_in) > 0){
-		sum_in = lapply( sumNm_in , function(colNm,wtNm,x,...) data.frame( sum(x[,colNm]*x[,wtNm],na.rm=T)), wtNm = tr_nms[["tr_wt"]] , x = tr_in )
+		sum_in = lapply( sum_nm_in , function(col_nm,wt_nm,x,...) data.frame( sum(x[,col_nm]*x[,wt_nm],na.rm=T)), wt_nm = tree_nms[["tr_wt"]] , x = tr_in )
 		sum_in = data.frame(matrix(unlist(sum_in),nrow = 1))
-		names(sum_in) = sumNm_in
+		names(sum_in) = sum_nm_in
 	}else{
-		sum_in = tr_in[1,sumNm_in]
-		sum_in[1,sumNm_in] = 0
+		sum_in = tr_in[1,sum_nm_in]
+		sum_in[1,sum_nm_in] = 0
 	}
 	return(sum_in)
 	gc()
@@ -482,25 +494,25 @@ plot_wtsum = function(
 #'@export
 #'@rdname compile_plots
 plot_wtmn = function(
-	trs
-	,tr_nms
+	dat_tree
+	,tree_nms
 	,...
 ){
 
 	#remove trees without ID fields
-	bad_ids = apply(is.na(trs[,tr_nms[["plot_ids"]],drop=F]),1,function(x) TRUE %in% x )
-	tr_in = trs[!bad_ids,]
+	bad_ids = apply(is.na(dat_tree[,tree_nms[["plot_ids"]],drop=F]),1,function(x) TRUE %in% x )
+	tr_in = dat_tree[!bad_ids,]
 
 	#correct for NA weights
-	tr_in[is.na(tr_in[,tr_nms[["dbh"]]]),] = 0
-	tr_in[is.na(tr_in[,tr_nms[["tr_wt"]]]),] = 0
+	tr_in[is.na(tr_in[,tree_nms[["dbh"]]]),] = 0
+	tr_in[is.na(tr_in[,tree_nms[["tr_wt"]]]),] = 0
 
 	if(nrow(tr_in) > 0){
 
 		wtmn = data.frame(
 			ntree = nrow(tr_in)
-			,qmd = sqrt(sum(tr_in[,tr_nms[["tr_wt"]]]*tr_in[,tr_nms[["dbh"]]]^2,na.rm=T) / sum(tr_in[,tr_nms[["tr_wt"]]],na.rm=T))
-			,lorht = sum(tr_in[,tr_nms[["ht"]]] * tr_in[,tr_nms[["tr_wt"]]] * tr_in[,tr_nms[["dbh"]]]^2,na.rm=T)/ sum(tr_in[,tr_nms[["tr_wt"]]] * tr_in[,tr_nms[["dbh"]]]^2,na.rm=T)
+			,qmd = sqrt(sum(tr_in[,tree_nms[["tr_wt"]]]*tr_in[,tree_nms[["dbh"]]]^2,na.rm=T) / sum(tr_in[,tree_nms[["tr_wt"]]],na.rm=T))
+			,lorht = sum(tr_in[,tree_nms[["ht"]]] * tr_in[,tree_nms[["tr_wt"]]] * tr_in[,tree_nms[["dbh"]]]^2,na.rm=T)/ sum(tr_in[,tree_nms[["tr_wt"]]] * tr_in[,tree_nms[["dbh"]]]^2,na.rm=T)
 		)
 
 	}else{
@@ -515,41 +527,41 @@ plot_wtmn = function(
 
 #'@export
 #'@rdname compile_plots
-#spp_y_plot = function(x,spp_y,sppNm,plot_ids,wtNm=NA,...){
+#spp_y_plot = function(x,spp_y,sppNm,plot_ids,wt_nm=NA,...){
 spp_y_plot = function(
-  trs
-  ,tr_nms
+  dat_tree
+  ,tree_nms
   ,n_dom_spp = 3
   ,spp_y
-  #x,sppNm,plot_ids,wtNm=NA,...
+  #x,sppNm,plot_ids,wt_nm=NA,...
   ,...
 ){
 
   require("reshape2")
 
   #remove trees without ID fields
-  bad_ids = apply(is.na(trs[,tr_nms[["plot_ids"]],drop=F]),1,function(x) TRUE %in% x )
+  bad_ids = apply(is.na(dat_tree[,tree_nms[["plot_ids"]],drop=F]),1,function(x) TRUE %in% x )
   if(sum(bad_ids) > 0){
-    warning("some records have bad plot_ids fields:", unique(trs[bad_ids,tr_nms[["plot_ids"]]]) )
+    warning("some records have bad plot_ids fields:", unique(dat_tree[bad_ids,tree_nms[["plot_ids"]]]) )
   }
-  tr_in = trs[!bad_ids,]
+  tr_in = dat_tree[!bad_ids,]
 
   #correct for NA weights
-  tr_in[is.na(tr_in[,tr_nms[["dbh"]]]),tr_nms[["dbh"]]] = 0
-  tr_in[is.na(tr_in[,tr_nms[["tr_wt"]]]),tr_nms[["tr_wt"]]] = 0
+  tr_in[is.na(tr_in[,tree_nms[["dbh"]]]),tree_nms[["dbh"]]] = 0
+  tr_in[is.na(tr_in[,tree_nms[["tr_wt"]]]),tree_nms[["tr_wt"]]] = 0
 
   #get data holder for results
-  res_in = tr_in[1,tr_nms[["plot_ids"]],drop=F]
+  res_in = tr_in[1,tree_nms[["plot_ids"]],drop=F]
 
   #iterate across response fields
-  for(i in 1:length(tr_nms[["domspp_y"]])){
+  for(i in 1:length(tree_nms[["domspp_y"]])){
 
     #compute weighted values
-    if(!is.na(tr_nms[["tr_wt"]])) tr_in[,tr_nms[["domspp_y"]][i]] = tr_in[,tr_nms[["domspp_y"]][i]] * tr_in[,tr_nms[["tr_wt"]]]
+    if(!is.na(tree_nms[["tr_wt"]])) tr_in[,tree_nms[["domspp_y"]][i]] = tr_in[,tree_nms[["domspp_y"]][i]] * tr_in[,tree_nms[["tr_wt"]]]
 
     #cast and aggregate
-    mi = reshape2::melt( tr_in[,c(tr_nms[["plot_ids"]],tr_nms[["spcd"]],tr_nms[["domspp_y"]][i]) ] , id.vars = c(tr_nms[["plot_ids"]],tr_nms[["spcd"]]) )
-    fi = as.formula(paste("variable  + ", paste(tr_nms[["plot_ids"]],collapse = "+")," ~ ",tr_nms[["spcd"]], sep=""))
+    mi = reshape2::melt( tr_in[,c(tree_nms[["plot_ids"]],tree_nms[["spcd"]],tree_nms[["domspp_y"]][i]) ] , id.vars = c(tree_nms[["plot_ids"]],tree_nms[["spcd"]]) )
+    fi = as.formula(paste("variable  + ", paste(tree_nms[["plot_ids"]],collapse = "+")," ~ ",tree_nms[["spcd"]], sep=""))
     dfi = reshape2::dcast( mi , formula =  fi , fun.aggregate = sum )[,-1]
     dfi1=dfi
 
@@ -557,17 +569,17 @@ spp_y_plot = function(
     n_dom = min(ncol(dfi1)-1, n_dom_spp)
     dom_order = order(dfi1[,-1] , decreasing = T)
     spp_nms = names(dfi)[-1]
-    nms_mx = paste("dom", tr_nms[["spcd"]], tr_nms[["domspp_y"]][i],1:n_dom, sep="_")
-    nms_mx_prop = paste("dom_prop", tr_nms[["spcd"]], tr_nms[["domspp_y"]][i],1:n_dom, sep="_")
+    nms_mx = paste("dom", tree_nms[["spcd"]], tree_nms[["domspp_y"]][i],1:n_dom, sep="_")
+    nms_mx_prop = paste("dom_prop", tree_nms[["spcd"]], tree_nms[["domspp_y"]][i],1:n_dom, sep="_")
     dfi[,nms_mx] = spp_nms[dom_order][1:n_dom]
 
     #get proportion by species
-    nms_mx_p = paste(spp_nms, tr_nms[["domspp_y"]][i],"p", sep="_")
+    nms_mx_p = paste(spp_nms, tree_nms[["domspp_y"]][i],"p", sep="_")
     y_ord_ndom = dfi1[,-1][dom_order][1:n_dom]
     dfi[,nms_mx_p] = y_ord_ndom / sum(y_ord_ndom)
 
     #merge data
-    res_in = merge(res_in, dfi[,c(tr_nms[["plot_ids"]],nms_mx,nms_mx_p)] , by=tr_nms[["plot_ids"]])
+    res_in = merge(res_in, dfi[,c(tree_nms[["plot_ids"]],nms_mx,nms_mx_p)] , by=tree_nms[["plot_ids"]])
 
   }
   return(res_in)
